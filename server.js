4663ad67ac7d0561f28cdc1496870c74197f021e
Israel Akpadja <drimmaculate147@gmail.com>
2024-11-04 14:00:18 +0000

    created request query

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const path = require("path");
+const port = process.env.PORT;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello!</h1>");
+// setup static folder
+// app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
+
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  console.log(req);
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
+    res.json(posts);
+  }
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h2>");
+// Get a single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.filter((post) => post.id === id));
 });
 
-app.listen(5000, () => console.log(`Server is running on port 8000.`));
+app.listen(5000, () => console.log(`Server is running on port ${port}.`));
