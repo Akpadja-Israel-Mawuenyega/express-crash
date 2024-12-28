b7670732e8aa50d9922bdd7d31a70860b83416b9
Israel Akpadja <drimmaculate147@gmail.com>
2024-12-28 15:16:29 +0000

    moved service worker to index.html

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -288,20 +288,3 @@ async function toggleTaskCompletion(taskId, status) {
 button.addEventListener("click", showTasks);
 deleteAllButton.addEventListener("click", deleteAllTasks);
 form.addEventListener("submit", addTask);
-
-// register service workers
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/client/public/js/sw.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  });
-}
