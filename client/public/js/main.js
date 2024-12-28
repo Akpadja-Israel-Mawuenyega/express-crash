ad71d12920b810d8fa199b2a3599e30a4e79426e
Israel Akpadja <drimmaculate147@gmail.com>
2024-12-28 15:10:51 +0000

    register sw.js

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -291,12 +291,17 @@ form.addEventListener("submit", addTask);
 
 // register service workers
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("../../../client/public/js/sw.js")
-    .then((registration) =>
-      console.log("Service worker registered:", registration)
-    )
-    .catch((registrationError) =>
-      console.error("Service worker registration failed:", registrationError)
-    );
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
 }
