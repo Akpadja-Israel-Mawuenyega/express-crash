9b38f86cc046f3804ad64a170a81a9d47eaf6505
Israel Akpadja <drimmaculate147@gmail.com>
2024-12-31 19:08:45 +0000

    bwhjjhwh

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -87,7 +87,7 @@ export const deleteTask = async (req, res) => {
 // @route POST /api/save-subscriptions
 export const createSubscription = async (req, res) => {
   try {
-    const newSubscription = await subscriptionMessage.create({ ...req.body });
+    const newSubscription = await subscriptionMessage.create(req.body);
     res.status(200).json(newSubscription);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -100,5 +100,7 @@ export const sendPushNotification = async (req, res) => {
   try {
     webpush.sendNotification(subscriptionMessage[0], "Hello!");
     res.status(200).json({ message: "Message sent to push service." });
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
