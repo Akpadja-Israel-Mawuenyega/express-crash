224ba548bea4444de5bd470458e6f8c01b5c8102
Israel Akpadja <drimmaculate147@gmail.com>
2024-12-31 18:33:14 +0000

    hhdhhdhdh

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -88,7 +88,7 @@ export const deleteTask = async (req, res) => {
 export const createSubscription = async (req, res) => {
   try {
     const newSubscription = await subscriptionMessage.create({ ...req.body });
-    res.status(200).json(newSubscription);
+    res.status(200).json({ message: "Subscription saved." });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
